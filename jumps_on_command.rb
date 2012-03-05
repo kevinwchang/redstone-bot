62d12c12083cfee3edf8fd501993aa83c76b5c3e
David Grayson <davidegrayson@gmail.com>
2012-03-04 23:48:49 -0800

    Made jumps_on_command.rb more easily customizable.

--- jumps_on_command.rb
+++ jumps_on_command.rb
@@ -1,13 +1,17 @@
-module JumpsOnCommand		
+module JumpsOnCommand
+	FallRate = 0.1
+	JumpRate = 0.2
+	JumpTime = 1
+
 	def update_position
 		if @jumping
 			@position[:on_ground] = 0
-			change_y 1
-			if Time.now - @jumping_start > 2
+			change_y JumpRate
+			if Time.now - @jumping_start > JumpTime
 				@jumping = false
 			end
 		else
-			fall 0.1
+			fall FallRate
 		end
 	end
 	
